956e6063a38d7b6ea1331a0037629e5f237dd5d8
Nick B <95590816+HardDrop1966@users.noreply.github.com>
2024-06-29 15:24:34 +0200

    FFT and IFFT

--- FFT/fft.h
+++ FFT/fft.h
@@ -1,46 +1,52 @@
 #ifndef FFT_H
 #define FFT_H
-#include <xtensa/tie/fft_tie.h>
 #include <stdint.h>
 #include <stdbool.h>
 #include <math.h>
-#include 		<string.h>
-#include		<stdbool.h>
+
+
 /* FIX_MPY() - fixed-point multiplication macro.
- This macro is a statement, not an expression (uses asm).
- BEWARE: make sure _DX is not clobbered by evaluating (A) or DEST.
- args are all of type fixed.
- Scaling ensures that 32767*32767 = 32767. */
+   This macro is a statement, not an expression (uses asm).
+   BEWARE: make sure _DX is not clobbered by evaluating (A) or DEST.
+   args are all of type fixed.
+   Scaling ensures that 32767*32767 = 32767. */
 #define FIX_MPY(DEST,A,B)       DEST = ((long)(A) * (long)(B))>>15
 
 #define N_WAVE          1024    /* dimension of Sinewave[] */
 #define LOG2_N_WAVE     10      /* log2(N_WAVE) */
 
-#ifndef fixed
-#define fixed short
-#endif
-
+typedef int16_t fixed;
 extern fixed Sinewave[N_WAVE];
-
-typedef struct cplx {
-	fixed R;fixed I;
-} cplx;
 
 //function prototypes
 fixed fix_mpy(fixed a, fixed b);
 int fix_fft(fixed *fr, fixed *fi, int m, int inverse);
-int fft_mul(fixed fr[], fixed fi[], int m, int inverse);
-int fft_FFT2(fixed fr[], fixed fi[], int m, int inverse);
-void GenerateCoefficients(cplx* __restrict out_coeff, const uint32_t N,
-		bool inverse);
-static inline uint32_t BitReverse(uint32_t n);
-uint32_t Prepare_Data(cplx* __restrict const x, const uint32_t N,
-		const uint32_t lg2_N, const uint32_t a_start);
-uint32_t Between_Shuffle(cplx* __restrict const x, const uint32_t N,
-		const uint32_t lg2_N, const uint32_t length);
-int fft_FFT8(cplx*__restrict f, int m, int inverse,
-		const cplx* __restrict coeffs);
-int fft_adv(cplx*__restrict f, int m, int inverse,
-		const cplx* __restrict coeffs, int time_decimation);
+
+
+typedef struct cplx
+{
+    fixed R;
+    fixed I;
+} cplx;
+
+void
+SWOpt_GenerateCoefficients(cplx* __restrict out_coeff, const uint32_t N, bool inverse);
+
+uint32_t
+SWOpt_ButterflyIterativeFft(cplx* __restrict x,
+                            uint32_t N,
+                            uint32_t lg2_N,
+                            bool inverse,
+                            const cplx* __restrict coeffs);
+
+void
+HWOpt_GenerateCoefficients(cplx* __restrict out_coeff, const uint32_t N, bool inverse);
+
+uint32_t
+HWOpt_ButterflyIterativeFft(cplx* __restrict const x,
+                            const uint32_t N,
+                            const uint32_t lg2_N,
+                            const bool inverse,
+                            const cplx* __restrict coeffs);
 
 #endif	//FFT_H
